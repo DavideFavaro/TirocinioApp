607fbbf65e75f13f3cc0982e916f0fd8e9de38ee
DavideFavaro <874161@stud.unive.it>
2021-09-27 18:32:18 +0200

    Creazione app
    Ancora non funziona al lancio di "seed()"  si ha "MethodError: Cannot `convert` an object of type PosLenString to an object of type Int64"

---
+++ WebApp/CopernicusData/app/resources/products/Products.jl
@@ -0,0 +1,94 @@
+module Products
+
+import SearchLight: AbstractModel, DbId
+import Base: @kwdef
+
+export Product
+
+@kwdef mutable struct Product <: AbstractModel
+  id::DbId = DbId()
+  uuid::String = ""
+  relpassnumber::Int = 0 #Nullable
+  relpassdirection::String = "" #Nullable
+  filename::String = ""
+  instrumentname::String = ""
+  timeliness::String = "" #Nullable
+  footprint::String = ""
+  size::String = ""
+  productlevel::String = "" #Nullable
+  relorbitdir::String = "" #Nullable
+  ingestiondate::String = ""
+  ecmwf::String = "" #Nullable
+  sensoroperationalmode::String = "" #Nullable
+  beginposition::String = ""
+  procfacilityname::String = "" #Nullable
+  orbitnumber::Int = 0
+  mode::String = "" #Nullable
+  platformname::String = ""
+  producttype::String = ""
+  orbitdirection::String = ""
+  format::String = ""
+  passdirection::String = "" #Nullable
+  procfacilityorg::String = "" #Nullable
+  relativeorbitnumber::Int = 0
+  pduduration::Int = 0 #Nullable
+  platformidentifier::String = ""
+  gmlfootprint::String = ""
+  endposition::String = ""
+  creationdate::String = "" #Nullable
+  onlinequalitycheck::String = ""
+  processingname::String = "" #Nullable
+  identifier::String = ""
+  passnumber::Int = 0 #Nullable
+  pdualongtrackcoord::Int = 0 #Nullable
+  instrumentshortname::String = ""
+  processinglevel::String = "" #Nullable
+  cloudcoverpercentage::Real = 0.0 #Nullable
+  lrmpercentage::Int = 0 #Nullable
+  openseapercentage::Int = 0 #Nullable
+  landpercentage::Int = 0 #Nullable
+  sarpercentage::Int = 0 #Nullable
+  closedseapercentage::Int = 0 #Nullable
+  continentalicepercentage::Int = 0 #Nullable
+  nbfire::Int = 0 #Nullable
+  lastpassdirection::String = "" #Nullable
+  lastrelpassnumber::Int = 0 #Nullable
+  lastrelorbitdirection::String = "" #Nullable
+  lastorbitdirection::String = "" #Nullable
+  lastrelativeorbitnumber::Int = 0 #Nullable
+  lastpassnumber::Int = 0 #Nullable
+  lastrelpassdirection::String = "" #Nullable
+  lastorbitnumber::Int = 0 #Nullable
+  pdutileid::String = "" #Nullable
+  productclass::String = "" #Nullable
+  polarisationmode::String = "" #Nullable
+  status::String = "" #Nullable
+  slicenumber::Int = 0 #Nullable
+  missiondatatakeid::Int = 0 #Nullable
+  swathidentifier::String = "" #Nullable
+  acquisitiontype::String = "" #Nullable
+  productconsolidation::String = "" #Nullable
+  unclassifiedpercentage::Real = 0.0 #Nullable
+  s2datatakeid::String = "" #Nullable
+  granuleidentifier::String = "" #Nullable
+  level1cpdiidentifier::String = "" #Nullable
+  vegetationpercentage::Real = 0.0 #Nullable
+  notvegetatedpercentage::Real = 0.0 #Nullable
+  processingbaseline::Real = 0.0 #Nullable
+  mediumprobacloudspercentage::Real = 0.0 #Nullable
+  datastripidentifier::String = "" #Nullable
+  generationdate::String = "" #Nullable
+  illuminationzenithangle::Real = 0.0 #Nullable
+  platformserialidentifier::String = "" #Nullable
+  waterpercentage::Real = 0.0 #Nullable
+  highprobacloudspercentage::Real = 0.0 #Nullable
+  illuminationazimuthangle::Real = 0.0 #Nullable
+  snowicepercentage::Real = 0.0 #Nullable
+  hv_order_tileid::String = "" #Nullable
+  tileid::String = "" #Nullable
+  datatakesensingstart::String = "" #Nullable
+  leapsecond::Int = 0 #Nullable
+  leapSecondOccurrence::String = "" #Nullable
+end
+
+end
