f884034e7a591e4dccfdd3fab0558c8db106db7c
DavideFavaro <874161@stud.unive.it>
2021-09-29 17:50:39 +0200

    Suddivisione del df in base a platformname

--- WebApp/CopernicusData/app/resources/products/ProductsController.jl
+++ WebApp/CopernicusData/app/resources/products/ProductsController.jl
@@ -3,10 +3,16 @@ module ProductsController
 using CSV, DataFrames, Genie, Genie.Renderer.Html, SearchLight, Stipple, StippleUI, StippleCharts, Products
 
 sPage = ""
-data = CSV.read("D:\\Documents and Settings\\DAVIDE-FAVARO\\My Documents\\GitHub\\TirocinioApp\\WebApp\\CopernicusData\\db\\seeds\\data.csv", DataFrame )
-Base.@kwdef mutable struct ProductArray <: ReactiveModel
-  products::R{DataTable} = DataTable(data)
-  data_page::DataTablePagination = DataTablePagination(rows_per_page=100)
+
+data = CSV.read( split( @__DIR__, "app" )[1] * "db\\seeds\\data.csv", DataFrame, limit=300 )
+
+dfs = [ filter( row -> row.platformname == "Sentinel-$i", data ) for i in 1:3 ]
+
+Base.@kwdef mutable struct ProductTable <: ReactiveModel
+  s1_products::R{DataTable} = DataTable( dfs[1] )
+  s2_products::R{DataTable} = DataTable( dfs[2] )
+  s3_products::R{DataTable} = DataTable( dfs[3] )
+  data_page::DataTablePagination = DataTablePagination(rows_per_page=33)
 end
 
 model = Stipple.init(ProductArray())
@@ -15,7 +21,28 @@ function ui()
   global model = Stipple.init(ProductArray())
   page(
     vm(ProductsController.model), class="container", [
-        Stipple.table( :products; pagination=:data_page )
+
+      row([
+        cell( class="st-module", [
+          h3("Sentinel-1")
+          Stipple.table( :s1_products; pagination=:data_page )
+        ])
+      ])
+
+      row([
+        cell( class="st-module", [
+          h3("Sentinel-2")
+          Stipple.table( :s2_products; pagination=:data_page )
+        ])
+      ])
+
+      row([
+        cell( class="st-module", [
+          h3("Sentinel-3")
+          Stipple.table( :s3_products; pagination=:data_page )
+        ])
+      ])
+      
     ]
   )
 end
